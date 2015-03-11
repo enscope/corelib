d080a3ee55c00114922ca92aaf13c3ea59c1c153
Miroslav Hudak <mhudak@enscope.com>
2015-03-11 16:47:57 +0100

    Added a method to return class name from FQN

--- src/Utils/ClassUtils.php
+++ src/Utils/ClassUtils.php
@@ -8,5 +8,18 @@
 			{
 				return (array_key_exists($interface, class_implements($class_name)));
 			}
+
+			public static function getClassName($obj)
+			{
+				if (!is_object($obj)
+					&& !is_string($obj))
+				{
+					return (false);
+				}
+
+				$fullClassName = is_object($obj) ? get_class($obj) : $obj;
+				$className = explode('\\', $fullClassName);
+				return (array_pop($className));
+			}
 		}
 	}
